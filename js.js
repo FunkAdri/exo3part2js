02b674a00eb556cc1bf08999d8692fc37d6135bd
FunkAdri <adrienhenry.fp@gmail.com>
2018-10-10 14:18:04 +0200

    mieux

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 window.onkeyup = function(event) {
+//chargement de la fenêtre avec la fonction event quand une touche de clavier est bougée
   if (event.keyCode) {
+//afficher une boîte de dialogue la valeur mise dans l'input lastname
     alert(lastname.value);
   }
 };
